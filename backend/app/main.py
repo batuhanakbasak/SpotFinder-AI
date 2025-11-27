b694a43c537ba8f26840c8dee7098b982683df54
Huseyin Lus <huseyinlus@Huseyins-MacBook-Air.local>
2025-11-27 16:10:30 +0100

    Setup for fastapi and a test route

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,1 +1,10 @@
-print("sa")
+from fastapi import FastAPI
+import app.routers.test_router as test_router
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello SpotFinder!"}
+
+app.include_router(test_router.router, prefix="/test", tags=["test"])
